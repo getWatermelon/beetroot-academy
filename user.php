acf0112833320e3bb22da8fee65328453682e734
Ivan <39527854+Ivan3228@users.noreply.github.com>
2020-04-18 17:47:55 +0300

    first commit

---
+++ user.php
@@ -0,0 +1,50 @@
+<?php
+$value = 'Hello from PHP 7.2';
+$name = 'Ivan';
+$surname = 'Petrov';
+$age = '28';
+$email = 'ivan-myasoyedov@stud.onu.edu.ua';
+var_dump($_POST);
+var_dump($_GET);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>
+<body>
+<br />
+<h1>Your Name <?php echo $_POST['name']?> and your gender <?php echo $_POST['gender'][0]?></h1>
+<div class="container">
+<form method="post" action="user.php">
+    <div class="form-group">
+        <label for="formGroupExampleInput">Имя</label>
+        <input type="text" class="form-control" id="formGroupExampleInput" name="name"  placeholder="Example input" value="<?=$_POST['name'] ?? 'Mike'?>">
+    </div>
+    <div class="form-group">
+        <label for="formGroupExampleInput">Фамилия</label>
+        <input type="text" class="form-control" id="formGroupExampleInput" name="surname" placeholder="Example input" value="<?=$_POST['surname'] ?? 'Petrov'?>">
+    </div>
+    <div class="form-group">
+        <label for="formGroupExampleInput">Возраст</label>
+        <input type="text" class="form-control" id="formGroupExampleInput" name="age" placeholder="Example input" value="<?=$_POST['age'] ?? '20'?>">
+    </div>
+    <div class="form-group">
+        <label for="formGroupExampleInput">Почта</label>
+        <input type="text" class="form-control" id="formGroupExampleInput"name="email" placeholder="Example input" value="<?=$_POST['email'] ?? 'ivan-myasoyedov@stud.onu.edu.ua'?>">
+    </div>
+    <div class="form-group">
+        <label for="exampleFormControlSelect1">Example select</label>
+        <select multiple class="form-control" id="exampleFormControlSelect1" name="gender[]">
+            <option>Man</option>
+            <option>Woman</option>
+            <option>Others</option>
+        </select>
+    </div>
+    <button type="submit" class="btn btn-primary">Отправить</button>
+</form>
+</div>
+</body>
+</html>
---
+++ user2.php
@@ -0,0 +1,54 @@
+<?php
+//$value = 'Hello from PHP 7.2';
+//$name = 'Ivan';
+//$surname = 'Petrov';
+//$age = '28';
+//$email = 'ivan-myasoyedov@stud.onu.edu.ua';
+var_dump($_POST);
+var_dump($_GET);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>
+<body>
+<br />
+<h1>Your Name <?php echo $_POST['name']?> and your gender <?php echo $_POST['gender'][0]?></h1>
+<div class="container">
+    <form method="post" action="user.php">
+        <div class="form-group">
+            <label for="formGroupExampleInput">Name</label>
+            <input type="text" class="form-control" id="formGroupExampleInput" name="name"  placeholder="Example input" value="<?=$_POST['name']?>">
+        </div>
+        <div class="form-group">
+            <label for="formGroupExampleInput">Surname</label>
+            <input type="text" class="form-control" id="formGroupExampleInput" name="surname" placeholder="Example input" value="<?=$_POST['surname']?>">
+        </div>
+        <div class="form-group">
+            <label for="formGroupExampleInput">Email</label>
+            <input type="email" class="form-control" id="formGroupExampleInput" name="email" placeholder="Example input" value="<?=$_POST['email']?>">
+        </div>
+        <div class="form-group">
+            <label for="formGroupExampleInput">Age</label>
+            <input type="number" class="form-control" id="formGroupExampleInput" name="age" placeholder="Example input" value="<?=$_POST['age']?>">
+        </div>
+        <div class="form-group">
+            <label for="formGroupExampleInput">Password</label>
+            <input type="password" class="form-control" id="formGroupExampleInput"name="password" placeholder="Example input" value="<?=$_POST['password']?>">
+        </div>
+        <div class="form-group">
+            <label for="exampleFormControlSelect1">Example select</label>
+            <select multiple class="form-control" id="exampleFormControlSelect1" name="gender[]">
+                <option>Man</option>
+                <option>Woman</option>
+                <option>Others</option>
+            </select>
+        </div>
+        <button type="submit" class="btn btn-primary">Register</button>
+    </form>
+</div>
+</body>
+</html>
