acf70c6387ebb98bd74025d42d9a71ac407ebf24
Ivan <39527854+Ivan3228@users.noreply.github.com>
2020-05-18 23:39:54 +0300

    ÑЭlast срфÑchanges 18.05.2020

--- bookstore/environment/bootstrap.sh
+++ bookstore/environment/bootstrap.sh
@@ -4,6 +4,7 @@ apt update
 apt install -y apache2
 apt install -y php
 apt install -y mysql-server
+apt install -y php-mysql
 apt install -y php-xdebug php-xml php-mbstring php-curl
 cp /var/www/html/environment/xdebug.ini /etc/php/7.2/cli/conf.d/20-xdebug.ini
 cp /var/www/html/environment/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
--- bookstore/index.php
+++ bookstore/index.php
@@ -1,5 +1,44 @@
-exi
 <?php
-phpinfo();
-exit;
+$pdo = new PDO("mysql:dbname=bookstore;host=127.0.0.1;charset=utf8mb4", 'root', '', [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+]);
 
+$query = 'SELECT b.id, b.title, g.`name`, a.`name` FROM bookstore.book AS b
+          left join bookstore.genre AS g ON b.genre_id  = g.id
+          left join bookstore.author AS a ON b.author_id  = a.id
+          ORDER BY b.id';
+
+$result = $pdo->query($query);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+    <h2>BookStore</h2>
+    <table class="table">
+        <tr>
+            <th>#</th>
+            <th>Title</th>
+            <th>Genre</th>
+            <th>Author</th>
+        </tr>
+        <tbody>
+        <?php foreach ($result as $key => $row) : ?>
+            <tr style="background-color: <?= ($key % 2 === 0) ? '#aaa' : '#fff' ?>">
+                <td><?= $row[0] ?></td>
+                <td><?= $row[1] ?></td>
+                <td><?= $row[2] ?></td>
+                <td><?= $row[3] ?></td>
+            </tr>
+        <?php endforeach; ?>
+        </tbody>
+    </table>
+</div>
+</body>
+</html>
