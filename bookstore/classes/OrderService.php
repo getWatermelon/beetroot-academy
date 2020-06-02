cf3e27cdfe3195124003f7b19c3e1209ddcb9fb2
Ivan <39527854+Ivan3228@users.noreply.github.com>
2020-06-03 02:46:49 +0300

    added to getBookIdByName function in OrderService class

--- bookstore/classes/OrderService.php
+++ bookstore/classes/OrderService.php
@@ -12,10 +12,11 @@ class OrderService
      */
     public function getOrders(): array
     {
-        $sql = "SELECT ob.order_id, group_concat(b.title separator ',') books, o.added_at, o.`status`, IFNULL(o.amount,0) amount  FROM bookstore.order_book ob
-                join bookstore.`order` as o on ob.order_id = o.order_id
-                join bookstore.book as b on ob.book_id = b.id
-                group by ob.order_id";
+        $sql = "SELECT ob.order_id, group_concat(b.title SEPARATOR ',') books, o.added_at, o.`status`, IFNULL(o.amount,0) amount  FROM bookstore.order_book ob
+                JOIN bookstore.`order` o ON ob.order_id = o.order_id
+                JOIN bookstore.book b ON ob.book_id = b.id
+                GROUP BY ob.order_id
+                ORDER BY ob.order_id DESC";
         $pdo = getPDO();
         $stmt = $pdo->query($sql);
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
